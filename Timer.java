51e13ac52a43bc5e4d210e4dc9562521ce694923
MichiTheGenius <litzinger04@gmail.com>
2022-06-21 09:10:25 +0200

    Timer geht jetzt bis 0 runter

--- Timer.java
+++ Timer.java
@@ -5,32 +5,44 @@ public class Timer {
     private int minuten;
     private int sekunden;
     private int frameCount;
+    private boolean ende;
 
-    public Timer(int schriftgrösse)
-    {
+    public Timer(int schriftgrösse) {
         this.schriftgrösse = schriftgrösse;
-        minuten = 2;
+        minuten = 0;
         sekunden = 10;
         frameCount = 0;
-    }
-    
-    public void zeichnen()
-    {
-        DrawText("0" + Integer.toString(minuten) + ":" + Integer.toString(sekunden), Einstellungen.bildschirmBreite - 200, 0, schriftgrösse, RED);
+        ende = false;
     }
 
-    public void tick()
-    {
-        if(frameCount == 60) // 1 Sekunde vergangen
-        {
-            sekunden--;
-            frameCount = 0;
+    public void zeichnen() {
+        String text1 = "0" + Integer.toString(minuten) + ":0" + Integer.toString(sekunden);
+        String text2 = "0" + Integer.toString(minuten) + ":" + Integer.toString(sekunden);
+        if (sekunden < 10) {
+            DrawText(text1,
+                    Einstellungen.bildschirmBreite - MeasureText(text1, schriftgrösse) - 5, 0, schriftgrösse, RED);
+        } else {
+            DrawText(text2,
+                    Einstellungen.bildschirmBreite - MeasureText(text2, schriftgrösse) - 5 , 0, schriftgrösse, RED);
         }
-        frameCount++;
-        if(sekunden == -1)
-        {
-            minuten--;
-            sekunden=59;
+    }
+
+    public void tick() {
+        if (!ende) {
+            if (frameCount == 60) // 1 Sekunde vergangen
+            {
+                sekunden--;
+                frameCount = 0;
+            }
+            frameCount++;
+            if (sekunden == -1) {
+                minuten--;
+                sekunden = 59;
+            }
+            if (minuten == 0 && sekunden == 0) {
+                System.out.println("Verloren!");
+                ende = true;
+            }
         }
     }
 }
